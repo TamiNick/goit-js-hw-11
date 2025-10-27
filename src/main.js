89310bc232f10db893bff23824f23d56906627a2
unknown <dmytriy.litvin@gmail.com>
2025-10-27 20:19:52 +0200

    done

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import { getImagesByQuery } from "./js/pixabay-api.js";
+import { createGallery, clearGallery, showLoader, hideLoader, } from "./js/render-functions.js";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const form = document.querySelector(".form");
+const input = form.querySelector("input[name='search-text']");
+
+form.addEventListener("submit", searcher);
+
+async function searcher(event) {
+    event.preventDefault();
+
+    const query = input.value.trim();
+    if (!query) {
+    iziToast.warning({
+        message: "Please enter a search term!",
+        position: "topRight",
+    });
+    return;
+    }
+
+    clearGallery();
+    showLoader();
+
+    try {
+    const data = await getImagesByQuery(query);
+
+    if (!data.hits || data.hits.length === 0) {
+        iziToast.error({
+        message: "Sorry, there are no images matching your search query. Please try again!",
+        position: "topRight",
+        });
+        return;
+    }
+
+    createGallery(data.hits);
+    } catch (error) {
+    iziToast.warning({
+        message: "Something went wrong. Please try again later.",
+        position: "topRight",
+    });
+    } finally {
+    hideLoader();
+    form.reset();
+    }
+}
+
+
+
